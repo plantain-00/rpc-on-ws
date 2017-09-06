b51a1cedf19ae87357b67a4391a94b6699fcf1d6
york yao <yaoao12306@outlook.com>
2017-09-06 15:10:18 +0800

    improve script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,5 +1,8 @@
 const childProcess = require('child_process')
 const { sleep } = require('clean-scripts')
+const util = require('util')
+
+const execAsync = util.promisify(childProcess.exec)
 
 module.exports = {
   build: [
@@ -33,19 +36,13 @@ module.exports = {
       await sleep(2000)
       server.kill('SIGINT')
     },
-    () => new Promise((resolve, reject) => {
-      childProcess.exec('git status -s', (error, stdout, stderr) => {
-        if (error) {
-          reject(error)
-        } else {
-          if (stdout) {
-            reject(new Error(`generated files doesn't match.`))
-          } else {
-            resolve()
-          }
-        }
-      }).stdout.pipe(process.stdout)
-    })
+    async () => {
+      const { stdout } = await execAsync('git status -s')
+      if (stdout) {
+        console.log(stdout)
+        throw new Error(`generated files doesn't match.`)
+      }
+    }
   ],
   fix: {
     ts: `tslint --fix "src/*.ts" "spec/*.ts"`,
