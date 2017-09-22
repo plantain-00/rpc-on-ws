cdb459cba43e6ea7384ff9362b2120b09195eefc
york yao <yaoao12306@outlook.com>
2017-09-22 15:37:38 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import uglify from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'dist/browser/index.js',
-  dest: 'dist/rpc-on-ws.min.js',
-  format: 'umd',
-  moduleName: 'WsRpc',
-  plugins: [resolve(), uglify()]
+  input: 'dist/browser/index.js',
+  name: 'WsRpc',
+  plugins: [resolve(), uglify()],
+  output: {
+    file: 'dist/rpc-on-ws.min.js',
+    format: 'umd'
+  }
 }
