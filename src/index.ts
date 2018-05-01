84edcacdc4c37d935057b76c6ea0f82b80300c65
york yao <yaoao12306@outlook.com>
2018-05-01 17:25:57 +0800

    build: add tslint-sonarts

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,25 @@ import 'rxjs/add/operator/filter'
  */
 export default class WsRpc<T> {
   private lastRequestId = 0
-  constructor (private subject: Subject<T>, private getRequestId: (message: T) => number | undefined, private getError: (message: T) => string | undefined, private timeout = 10000) { }
-  public send (send: (requestId: number) => void, timeout?: number) {
+  constructor(private subject: Subject<T>, private getRequestId: (message: T) => number | undefined, private getError: (message: T) => string | undefined, private timeout = 10000) { }
+  public send(send: (requestId: number) => void, timeout?: number) {
     return new Promise<T>((resolve, reject) => {
       const requestId = this.generateRequestId()
       let timeoutId: NodeJS.Timer
       const subscription = (this.subject as Observable<T>)
-                .filter((r: T) => this.getRequestId(r) === requestId)
-                .subscribe((r: T) => {
-                  if (timeoutId) {
-                    clearTimeout(timeoutId)
-                  }
-                  subscription.unsubscribe()
-                  const error = this.getError(r)
-                  if (error) {
-                    reject(new Error(error))
-                  } else {
-                    resolve(r)
-                  }
-                })
+        .filter((r: T) => this.getRequestId(r) === requestId)
+        .subscribe((r: T) => {
+          if (timeoutId) {
+            clearTimeout(timeoutId)
+          }
+          subscription.unsubscribe()
+          const error = this.getError(r)
+          if (error) {
+            reject(new Error(error))
+          } else {
+            resolve(r)
+          }
+        })
       timeoutId = setTimeout(() => {
         subscription.unsubscribe()
         reject(new Error('timeout'))
@@ -33,7 +33,7 @@ export default class WsRpc<T> {
       send(requestId)
     })
   }
-  public generateRequestId () {
+  public generateRequestId() {
     this.lastRequestId = this.lastRequestId < 4294967295 ? this.lastRequestId + 1 : 1 // 4294967295 = 2^32 - 1
     return this.lastRequestId
   }
