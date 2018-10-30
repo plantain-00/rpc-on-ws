06ad960fe6941e3338b0098a382649d20e66e22f
york yao <yaoao12306@outlook.com>
2018-10-30 23:03:37 +0800

    chore: add type-coverage check

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from 'rollup-plugin-node-resolve'
 export default {
   input: 'dist/browser/index.js',
   name: 'WsRpc',
-  plugins: [resolve(), uglify()],
+  plugins: [resolve({ browser: true }), uglify()],
   output: {
     file: 'dist/rpc-on-ws.min.js',
     format: 'umd'
